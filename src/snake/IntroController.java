3944d49fc4a7eb5b3352458d08b34ee70ed01069
Paweł Dąbrowski <fluorix@gmail.com>
2021-06-30 00:07:09 +0200

    Add intro screen with option to display menu.

---
+++ src/snake/IntroController.java
@@ -0,0 +1,35 @@
+package snake;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class IntroController {
+
+    @FXML
+    Button welcomeButton;
+
+    public void exit() {
+        Platform.exit();
+    }
+
+    public void showMenu() throws IOException {
+        Scene currentScene = welcomeButton.getScene();
+        Stage currentWindow = (Stage) currentScene.getWindow();
+
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        Scene nextScene = new Scene(root, 800, 600);
+        currentWindow.setScene(nextScene);
+
+    }
+
+    public void startGame() {
+
+    }
+}
