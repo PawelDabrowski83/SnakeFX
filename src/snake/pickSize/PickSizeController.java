25690a355bf30586149e314f924d48dd323522e6
Paweł Dąbrowski <fluorix@gmail.com>
2021-07-02 00:06:02 +0200

    Implement universal method for field size creation.

--- src/snake/pickSize/PickSizeController.java
+++ src/snake/pickSize/PickSizeController.java
@@ -1,7 +1,9 @@
 package snake.pickSize;
 
-import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Dimension2D;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import snake.baseController.BaseController;
 
@@ -12,6 +14,7 @@ public class PickSizeController extends BaseController {
     public static final int MAX_SIDE_HEIGHT = 15;
     public static final int MIN_SIDE_WIDTH = 5;
     public static final int MIN_SIDE_HEIGHT = 5;
+    public static final String ILLEGAL_SIDE_BUTTON = "Unexpected value on pickSize button. Contact project owner.";
 
     @FXML
     public TextField sideWidth;
@@ -27,16 +30,32 @@ public class PickSizeController extends BaseController {
         return false;
     }
 
-    public void randomizeSides() {
-        sideWidth.setText(
-                String.valueOf(
-                        ThreadLocalRandom.current().nextInt(MIN_SIDE_WIDTH, MAX_SIDE_WIDTH)
-                )
-        );
-        sideHeight.setText(
-                String.valueOf(
-                        ThreadLocalRandom.current().nextInt(MIN_SIDE_HEIGHT, MAX_SIDE_HEIGHT)
-                )
-        );
+    public void setSides(ActionEvent event) {
+        Button button = (Button) event.getSource();
+        String option = button.getText();
+        Dimension2D fieldSize = switch(option) {
+            case "Small" -> new Dimension2D(MIN_SIDE_WIDTH, MIN_SIDE_HEIGHT);
+            case "Medium" -> new Dimension2D((MAX_SIDE_WIDTH - MIN_SIDE_WIDTH) / 3 * 2, (MAX_SIDE_WIDTH - MIN_SIDE_WIDTH) / 3 * 2);
+            case "Large" -> new Dimension2D(MAX_SIDE_WIDTH, MAX_SIDE_HEIGHT);
+            case "Random" -> new Dimension2D(ThreadLocalRandom.current().nextInt(MIN_SIDE_WIDTH, MAX_SIDE_WIDTH), ThreadLocalRandom.current().nextInt(MIN_SIDE_HEIGHT, MAX_SIDE_HEIGHT));
+            default -> throw new IllegalStateException(ILLEGAL_SIDE_BUTTON);
+        };
+//        int width = switch(option) {
+//            case "Small" -> MIN_SIDE_WIDTH;
+//            case "Medium" -> (MAX_SIDE_WIDTH - MIN_SIDE_WIDTH) / 3 * 2;
+//            case "Large" -> MAX_SIDE_WIDTH;
+//            case "Random" -> ThreadLocalRandom.current().nextInt(MIN_SIDE_WIDTH, MAX_SIDE_WIDTH);
+//            default -> throw new IllegalStateException(ILLEGAL_SIDE_BUTTON);
+//        };
+//        int height = switch (option) {
+//            case "Small" -> MIN_SIDE_HEIGHT;
+//            case "Medium" -> (MAX_SIDE_HEIGHT - MIN_SIDE_HEIGHT) / 3 * 2;
+//            case "Large" -> MAX_SIDE_HEIGHT;
+//            case "Random" -> ThreadLocalRandom.current().nextInt(MIN_SIDE_HEIGHT, MAX_SIDE_HEIGHT);
+//            default -> throw new IllegalStateException(ILLEGAL_SIDE_BUTTON);
+//        };
+        sideWidth.setText(String.valueOf((int) fieldSize.getWidth()));
+        sideHeight.setText(String.valueOf((int) fieldSize.getHeight()));
     }
+
 }
