d0677fff51ae10dca1e817fdfd156b8705749c07
Paweł Dąbrowski <fluorix@gmail.com>
2021-07-02 10:00:28 +0200

    Try to get arrows working.

--- src/snake/field/FieldController.java
+++ src/snake/field/FieldController.java
@@ -1,5 +1,10 @@
 package snake.field;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,6 +15,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 import snake.baseController.BaseController;
 
 import java.net.URL;
@@ -18,12 +24,11 @@ import java.util.ResourceBundle;
 
 public class FieldController extends BaseController implements Initializable {
 
-    @FXML
     Circle head;
     @FXML
     GridPane grid;
 
-    double velocity = 1000;
+    double velocity = 3;
     boolean running = true;
     Direction direction = Direction.RIGHT;
 
@@ -37,18 +42,28 @@ public class FieldController extends BaseController implements Initializable {
     }
 
     public void run() {
-        Circle circle = new Circle(22);
+        head = new Circle(22);
         grid.setPrefSize(100, 100);
-        circle.setFill(Color.MEDIUMPURPLE);
-        grid.add(circle, 10, 10);
+        head.setFill(Color.MEDIUMPURPLE);
+        grid.add(head, 10, 10);
 
+        grid.setOnKeyPressed(e ->
+        {
+            switch(e.getCode()) {
+                case UP: direction = Direction.UP;
+                case DOWN: direction = Direction.DOWN;
+                case LEFT: direction = Direction.LEFT;
+                case RIGHT: direction = Direction.RIGHT;
+                case Q: exit();
+            }
+        });
 
         // game loop
         while(running) {
             try {
-
                 Thread.sleep(100);
                 move();
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -57,7 +72,11 @@ public class FieldController extends BaseController implements Initializable {
     }
 
     public void move() {
-//        System.out.println("move");
+        double stepX = head.getCenterX() + velocity;
+        head.setCenterX(stepX);
+        head.setTranslateX(stepX);
+        System.out.println(direction);
+
     }
 
     public void moveKey(KeyEvent event) {
