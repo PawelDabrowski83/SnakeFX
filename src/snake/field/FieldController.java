36925df61cc7e0dbc35faae51fc0780fd3d91560
Paweł Dąbrowski <fluorix@gmail.com>
2021-07-02 11:21:15 +0200

    Clean up unused imports.

--- src/snake/field/FieldController.java
+++ src/snake/field/FieldController.java
@@ -1,23 +1,12 @@
 package snake.field;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Cell;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-import javafx.util.Duration;
 import snake.baseController.BaseController;
 
 import java.net.URL;
