26a3ba6d4e74aae4ce80828d2eee2190ed73417c
Paweł Dąbrowski <fluorix@gmail.com>
2021-07-02 09:24:10 +0200

    Add snake's head. Set up direction enum for movement values.

---
+++ src/snake/field/Direction.java
@@ -0,0 +1,5 @@
+package snake.field;
+
+public enum Direction {
+    LEFT, RIGHT, UP, DOWN
+}
--- src/snake/field/FieldController.java
+++ src/snake/field/FieldController.java
@@ -3,14 +3,18 @@ package snake.field;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Cell;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import snake.baseController.BaseController;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 
 public class FieldController extends BaseController implements Initializable {
 
@@ -20,6 +24,8 @@ public class FieldController extends BaseController implements Initializable {
     GridPane grid;
 
     double velocity = 1000;
+    boolean running = true;
+    Direction direction = Direction.RIGHT;
 
     @Override
     @FXML
@@ -31,8 +37,14 @@ public class FieldController extends BaseController implements Initializable {
     }
 
     public void run() {
+        Circle circle = new Circle(22);
+        grid.setPrefSize(100, 100);
+        circle.setFill(Color.MEDIUMPURPLE);
+        grid.add(circle, 10, 10);
+
+
         // game loop
-        while(true) {
+        while(running) {
             try {
 
                 Thread.sleep(100);
@@ -45,10 +57,14 @@ public class FieldController extends BaseController implements Initializable {
     }
 
     public void move() {
-        System.out.println("move");
+//        System.out.println("move");
     }
 
     public void moveKey(KeyEvent event) {
         System.out.println(event.getText());
+    }
+
+    public void stop() {
+        running = false;
     }
 }
