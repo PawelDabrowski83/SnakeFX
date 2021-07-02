ec6378e3c8693035eace33993bf524f9007dde4d
Paweł Dąbrowski <fluorix@gmail.com>
2021-07-02 08:22:49 +0200

    Trying to get snake moving.

--- src/snake/field/FieldController.java
+++ src/snake/field/FieldController.java
@@ -1,6 +1,27 @@
 package snake.field;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Circle;
 import snake.baseController.BaseController;
 
-public class FieldController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FieldController extends BaseController implements Initializable {
+
+    @FXML
+    Circle head;
+
+    @Override
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        head.setCenterX(head.getCenterX() + 10);
+    }
+
+    public void move(ActionEvent event) {
+        System.out.println(event.toString());
+    }
 }
