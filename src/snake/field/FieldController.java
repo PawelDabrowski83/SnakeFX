b8708ed39e4253b11504e2078e0abbca1cf3d567
Paweł Dąbrowski <fluorix@gmail.com>
2021-07-02 08:40:16 +0200

    Empty field to have circle positioned by controller on initialize.

--- src/snake/field/FieldController.java
+++ src/snake/field/FieldController.java
@@ -4,9 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 import snake.baseController.BaseController;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,14 +16,39 @@ public class FieldController extends BaseController implements Initializable {
 
     @FXML
     Circle head;
+    @FXML
+    GridPane grid;
+
+    double velocity = 1000;
 
     @Override
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        head.setCenterX(head.getCenterX() + 10);
+        System.out.println("initialize");
+        Thread thread = new Thread(this::run);
+        thread.start();
+
     }
 
-    public void move(ActionEvent event) {
-        System.out.println(event.toString());
+    public void run() {
+        // game loop
+        while(true) {
+            try {
+
+                Thread.sleep(100);
+                move();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void move() {
+        System.out.println("move");
+    }
+
+    public void moveKey(KeyEvent event) {
+        System.out.println(event.getText());
     }
 }
